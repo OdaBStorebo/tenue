66139ed994da9cd857fe217bd9a4347d89fee1bf
Oda Bastesen Storebø <oda.bastesen.storebo@gmail.com>
2026-09-03 15:34:46 +0200

    Added Icons to navigation, fixed grid gap

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,15 +1,19 @@
 import styles from './Navigation.module.css';
+import hangerIcon from '../../assets/icons/hanger.svg';
+import outfitIcon from '../../assets/icons/outfit.svg';
+import laundryIcon from '../../assets/icons/laundry-basket.svg';
+import profileIcon from '../../assets/icons/profile.svg';
 
 
 export function Navigation() {
     return(
         <nav className={styles.nav}>
             <ul className={styles.navList}>
-                <li>Wardrobe</li>
-                <li>Outfit</li>
-                <li>Create New Item</li>
-                <li>Laundry</li>
-                <li>Profile</li>
+                <li><img src={hangerIcon} alt="Hanger" className={styles.icon}/>Wardrobe</li>
+                <li><img src={outfitIcon} alt="Outfit" className={styles.icon}/>Outfit</li>
+                <li><button className={styles.addButton}>+</button></li>
+                <li><img src={laundryIcon} alt="Laundry" className={styles.icon}/>Laundry</li>
+                <li><img src={profileIcon} alt="Profile" className={styles.icon}/>Profile</li>
             </ul>
         </nav>
     )
